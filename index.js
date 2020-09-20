5ab49d178c8a51e68e95b766b739645b7a76ce07
Mark McKelvy <mmckelvy@gmail.com>
2020-09-20 15:07:08 -0700

    handle the export

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ module.exports = {
   snakeCase: require('./src/snake-case'),
   camelCase: require('./src/camel-case'),
   snakeCaseKeys: require('./src/snake-case-keys'),
-  camelCaseKeys: require('./src/camel-case-keys')
+  camelCaseKeys: require('./src/camel-case-keys'),
+  properCase: require('./src/proper-case')
 };
